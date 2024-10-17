d73092c780ecae9ed3bb58c272eb9adca09b85a2
Lea-Antonia <lea1103@outlook.com>
2024-10-17 16:11:42 +0200

    added append method in model average

--- correlatoranalyser/model_average.py
+++ correlatoranalyser/model_average.py
@@ -1,19 +1,31 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Self, List, Dict
-from .fit import FitResult
+from fit import FitResult
 
 
 @dataclass
 class Utility:
     # list of fit results
     fit_results: List[FitResult] = field(default_factory=lambda: [])
-    num_states: int
+    # num_states: int
+    # keys_a
+    param_avg: dict = field(default_factory=dict)
+    param_avg_bst: dict = field(default_factory=dict)
 
-    def model_average(key: str = None) -> Dict:
-        # do fit for different timeslices, different nstates with fit.py
-        # sort by AIC value
-        # calculate the final parameter
+    def append(self, new_fit: FitResult) -> None:
+        self.fit_results.append(new_fit)
+        # ToDo: sort by AIC
+        self.fit_results.sort(key=lambda x: x.AIC)
+        return
+
+    def model_average(self, key: str = None) -> None:
+        def avg_single_key(key: str) -> None:
+            return
+
+        def avg_single_key_bst(key: str) -> None:
+            return
+
         return
 
     """method that gets a fit result based on the index in the list"""
