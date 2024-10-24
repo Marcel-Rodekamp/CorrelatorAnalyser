2fee4467b14f3009168e93948002fb516a4e219e
Lea-Antonia <lea1103@outlook.com>
2024-10-24 12:17:51 +0200

    completed model_average methos

--- correlatoranalyser/ExpSumFit.py
+++ correlatoranalyser/ExpSumFit.py
@@ -1,29 +1,48 @@
 import numpy as np
 import gvar as gv
-from model_average import Utility
+from model_average import FitState
 from fit import fit
 import matplotlib.pyplot as plt
 from fitModels import SimpleSumOfExponentialsModel, SimpleSumOfExponentialsFlatPrior
 
+
+"""
+ToDo: change model s.t. the order of parameters is sorted, with current Model only central value model averaging is working
+
+ """
 Nt = 16
 Nbst = 100
 Nconf = 100
-num_states = 2  # number of states
+num_states = 1  # number of states
 abscissa: np.ndarray = np.arange(0, Nt)
+
+real_abs = np.exp(-0.2 * abscissa)
+
 data: np.ndarray[gv.GVar] = gv.gvar(
-    np.exp(-0.2 * abscissa), 0.1 * np.exp(0.001 * abscissa)
+    np.random.normal(
+        real_abs,  # + 0.1 * np.exp(-0.35 * abscissa),
+        0.01 * np.exp(0.01 * abscissa),
+        size=(Nt),
+    ),
+    0.05 * np.exp(0.1 * abscissa),
 )
 data2 = np.random.normal(
-    np.exp(-0.2 * abscissa), 0.1 * np.exp(0.001 * abscissa), size=(Nconf, Nt)
+    real_abs,  # + 0.1 * np.exp(-0.35 * abscissa),
+    0.01 * np.exp(0.1 * abscissa),
+    size=(Nconf, Nt),
 )
+
 data_bst = np.zeros((Nbst, Nt))
 for nbst in range(Nbst):
     data_bst[nbst] = np.mean(data2[np.random.randint(0, Nconf, size=(Nconf,))], axis=0)
 
+plt.plot(abscissa, gv.mean(data), marker=".", ls="", color="b")
 # plt.errorbar(abscissa,gv.mean(data2[0]),gv.sdev(data2[0]))
 # plt.plot(abscissa, 1*np.exp(-abscissa*0.2)+0.1)
 # plt.show()
-res = Utility()
+
+
+res = FitState()
 te = abscissa[-1]
 for ns in range(1, num_states + 1):
     model = SimpleSumOfExponentialsModel(
@@ -31,7 +50,6 @@ for ns in range(1, num_states + 1):
     )  # lambda t, p: p["A0"] * np.exp(-t * p["E0"])
     # ToDo: refresh priors
     prior_new = SimpleSumOfExponentialsFlatPrior(Nstates=ns)()
-    # print(prior_new)
     # prior_new = {"E0": gv.gvar(0.5,100), #flat prior
     #                 "A0": gv.gvar(0.5,100),
     #             "E1": gv.gvar(0.5,100), #flat prior
@@ -51,12 +69,53 @@ for ns in range(1, num_states + 1):
             bootstrap_fit_correlated=True,
             # central_value_fit=False,
         )
+        """Make sure that A0>A1>...>An s.t. over the same parameter is averaged"""
+        print("Before sorting:", update.best_fit_param)
+        pairs = [
+            (update.best_fit_param[f"E{i}"], update.best_fit_param[f"A{i}"])
+            for i in range(ns)
+        ]
+        sorted_pairs = sorted(pairs, key=lambda x: gv.mean(x[0]), reverse=False)
+        for i, (E, A) in enumerate(sorted_pairs):
+            update.best_fit_param[f"E{i}"] = E
+            update.best_fit_param[f"A{i}"] = A
+        print("After sorting:", update.best_fit_param)
+
+        # print(update.best_fit_param_bst)
         res.append(update)
-        print("all keys for averaging:", res.keys_all)
+        # print("all keys for averaging:", res.keys_all)
+        print(f"...Averaging over {res.keys_all}")
         res.model_average()
 
+
+print(f"Result Model Averaging:")
+for key in res.keys_all:
+    value = res.param_avg[key + "_est"]
+    print(f"{key}_est: {value}")
+
+
+print("Top 10 Fit Results:")
 for i, fit in enumerate(res.fit_results):
-    print(
-        f"Fit {i}: range[{fit.ts},{fit.te}] with parameters {fit.best_fit_param} and AIC {fit.AIC}\n"
-    )
-# print(res)
+    if False:  # i > 10:
+        pass
+    else:
+        print(
+            f"Fit {i}: range[{fit.ts},{fit.te}] with parameters {fit.best_fit_param} (cv)  and AIC {fit.AIC}\n"
+        )
+
+plt.plot(
+    abscissa,
+    real_abs,
+    color="green",
+    label="real test data",
+)
+plt.plot(
+    abscissa,
+    gv.mean(res.param_avg["A0_est"])
+    * np.exp(-gv.mean(res.param_avg["E0_est"]) * abscissa),
+    color="red",
+    label="central value model average",
+)
+
+plt.legend()
+plt.show()
--- correlatoranalyser/model_average.py
+++ correlatoranalyser/model_average.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Self, List, Dict
-from .fit import FitResult
+from fit import FitResult
 import numpy as np
 import gvar as gv
 
@@ -13,16 +13,20 @@ class FitState:
     )  # list of fit results
     keys_all: list[str] = field(default_factory=list[str])
     param_avg: dict = field(default_factory=dict)
-    # param_avg_bst: dict = field(default_factory=dict)
 
     def append(self, new_fit: FitResult) -> None:
         self.fit_results.append(new_fit)
         # ToDO: add all parameters from the fit to the keys list
         try:
             new_keys = list(new_fit.best_fit_param.keys())
-            # print(new_keys)
             self.keys_all += [key for key in new_keys if key not in self.keys_all]
             self.fit_results.sort(key=lambda x: x.AIC)  # sort by AIC
+        except:
+            pass
+        try:
+            new_keys = list(new_fit.best_fit_param_bst.keys())
+            self.keys_all += [key for key in new_keys if key not in self.keys_all]
+            self.fit_results.sort(key=lambda x: x.AIC_bst)  # sort by AIC
         except:
             pass
 
@@ -35,15 +39,66 @@ class FitState:
         N = len(self.fit_results)
 
         def avg_single_key(key: str) -> None:
+            # prepare arrays for data:
+            param_est = np.zeros(N)
+            param_err = np.zeros(N)
+            AIC_est = np.zeros(N)
+            if self.fit_results[0].Nbst is not None:
+                Nbst = self.fit_results[0].Nbst
+                AIC_bst = np.zeros((Nbst, N))
+                param_bst = np.zeros((Nbst, N))
+                param_bst_err = np.zeros((Nbst, N))
 
+            for fitID, fit in enumerate(self.fit_results):
+                # check if data is bootstraped or/and from central value fit:
+                try:
+                    bestParam = fit.best_fit_param[key]
+                    param_est[fitID] = gv.mean(bestParam)
+                    param_err[fitID] = gv.sdev(bestParam)
+                    AIC_est[fitID] = fit.AIC
+                    # cv_flag = True
+                except TypeError:  # no central value fit, parameter is None
+                    pass
+                except KeyError:  # key is not in fit
+                    pass
+                try:
+                    bestParam_bst = fit.best_fit_param_bst[key]
+                    param_bst[:, fitID] = gv.mean(bestParam_bst)
+                    param_bst_err[:, fitID] = gv.sdev(bestParam_bst)
+                    AIC_bst[:, fitID] = fit.AIC_bst
+                except TypeError:  # no bootstrap fit, parameter is None
+                    pass
+                except KeyError:  # key is not in fit
+                    pass
+
+            # do model average for key for central value fit results
+            if self.fit_results[0].AIC is not None:  # check if central value fit
+                weights_est = np.exp(-0.5 * (AIC_est - np.min(AIC_est)))
+                modelAvg_est = np.average(param_est, weights=weights_est)
+                modelAvg_err = np.average(param_err, weights=weights_est)
+                self.param_avg[key + "_est"] = gv.gvar(modelAvg_est, modelAvg_err)
+            # model averaging for bootstrap fit results
+            if self.fit_results[0].Nbst is not None:  # check if bootstrap fit
+                weights_bst = np.exp(-0.5 * (AIC_bst - np.min(AIC_bst)))
+                modelAvg_bst = np.average(param_bst, weights=weights_bst, axis=1)
+                modelAvg_bst_err = Nbst * [np.std(modelAvg_bst, axis=0)]
+                self.param_avg[key + "_bst"] = gv.gvar(modelAvg_bst, modelAvg_bst_err)
             return
 
         # go through all parameters and do model averaging for each of them:
         if type(keys) is list:
             for key in keys:
+                if key not in self.keys_all:
+                    raise KeyError(
+                        f'The given key "{key}" is not a fit parameter, choose one parameter from {self.keys_all} for the model average.'
+                    )
                 avg_single_key(key=key)
         elif type(keys) is str:
-            avg_single_key(key=key)
+            if keys not in self.keys_all:
+                raise KeyError(
+                    f'The given key "{keys}" is not a fit parameter, choose one parameter from {self.keys_all} for the model average.'
+                )
+            avg_single_key(key=keys)
         else:
             for key in self.keys_all:
                 avg_single_key(key=key)
