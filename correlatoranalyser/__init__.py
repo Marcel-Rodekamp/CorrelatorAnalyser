90c74c3f9f3719803f17fd92d347153db9480309
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2024-11-25 12:23:09 +0100

    Updated main __init__

--- correlatoranalyser/__init__.py
+++ correlatoranalyser/__init__.py
@@ -1,1 +1,9 @@
+# Class that stores/organizes results of fits
+# result of fit(...)
+from .fitResult import FitResult
+
+# Collection of FitResults. It allows to simplify model averaging etc.
+from .fitState import FitState
+
+# A method that with provided data performs fit and return a FitResult
 from .fit import fit
