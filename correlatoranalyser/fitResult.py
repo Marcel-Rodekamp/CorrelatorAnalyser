1db37e89e50178098cd4bf8ccfed3d717fd3aa61
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2025-01-17 08:35:46 +0100

    Added ca.linear_regression, and import for fit_result

--- correlatoranalyser/fitResult.py
+++ correlatoranalyser/fitResult.py
@@ -12,7 +12,7 @@ from collections.abc import Callable
 
 import lsqfit
 
-
+import scipy.stats
 
 @dataclass
 class FitResult:
@@ -434,7 +434,56 @@ class FitResult:
         return out
 
 
+    def calculate_AIC(self, chi2, small_sample_correction: bool = True):
+        r"""
+            @param small_sample_correction: bool, flag to add/remove a small sample correction (default: False)
 
+            Compute the Akaike information criterion for a fit result
+            based on the chi^2 obtained from lsqfit.
+            The form can be found in
+                https://arxiv.org/abs/2305.19417
+                https://arxiv.org/abs/2208.14983
+                https://arxiv.org/abs/2008.01069
+            equation 3 in the first:
+                AIC = -2ln L^* + 2k - 2d_K 
+
+                AIC_augmented = AIC + \sum_{\Theta} ( \Theta - p_\Theta )**2/var(p_\Theta) 
+
+            A small sample correction can be applied to both by seeting small_sample_correction 
+            https://en.wikipedia.org/wiki/Akaike_information_criterion#Modification_for_small_sample_size
+                AICc = AIC + (2k^2 + 2k)/(d_K - k -1)
+
+            Here we compare
+                1. -2*ln(L^*) = chi^2
+                2. k = number of parameters
+                3. d_K = number of points
+                5. Model parameter \Theta
+                4. p_\Theta prior for parameter \Theta
+        """
+        if bool(self.best_fit_param):
+            Nparam: int = len(self.best_fit_param.keys())
+        elif bool(self.best_fit_param_res):
+            Nparam: int = len(self.best_fit_param_res.keys())
+        else:
+            raise RuntimeError("FitResult not initialized, can not determine number of parameters for calculating AIC")
+
+        Ndata = self.te - self.ts
+
+        # start with the degree of freedom. 
+        # TODO: This factor 2 is highly debated as it is very aggressive for many data sets
+        #       We may want to come up with a way to allow a more flexible way of calculating
+        #       the AIC.
+        #       For reference see issue #8
+        AIC: float = 2 * (Ndata-Nparam)
+
+        if small_sample_correction:
+            # This corrections is negligible if Ndata >> Nparam**2 and thus often very useful
+            # it effectively favours models with less parameters
+            AIC += (2*Nparam**2 + 2*Nparam)/(Ndata - Nparam - 1)
+
+        AIC += chi2
+
+        return AIC
 
     # ###########################################
     # Importers
@@ -479,27 +528,12 @@ class FitResult:
                 5. Model parameter \Theta
                 4. p_\Theta prior for parameter \Theta
         """
-        Nparam: int = len(nlf.p)
-        Ndata: int  = len(nlf.x)
-
-        # start with the degree of freedom. 
-        # TODO: This factor 2 is highly debated as it is very aggressive for many data sets
-        #       We may want to come up with a way to allow a more flexible way of calculating
-        #       the AIC.
-        #       For reference see issue #8
-        AIC: float = 2 * (Nparam - Ndata)
-
-        if small_sample_correction:
-            # This corrections is negligible if Ndata >> Nparam**2 and thus often very useful
-            # it effectively favours models with less parameters
-            AIC += (2*Nparam**2 + 2*Nparam)/(Ndata - Nparam - 1)
-
         # by default lsqfit includes priors to the chi^2 (if porvided) 
         if augmented:
-            AIC += nlf.chi2
+            AIC = self.calculate_AIC(nlf.chi2, small_sample_correction)
         # for non-augmented we have to explicitly recalculate chi^2
         else:
-            AIC += gv.chi2( nlf.y, nlf.fcn( nlf.x, nlf.p ) )
+            AIC = self.calculate_AIC(gv.chi2( nlf.y, nlf.fcn( nlf.x, nlf.p ) ), small_sample_correction)
 
         return AIC
     
@@ -507,7 +541,7 @@ class FitResult:
         r"""
             @param nlt: lsqfit.nonlinear_fit, Fit result from lsqfit. It stores all relevant information to
                                                compute the chi^2
-            @param augmented: bool, flag to calculate the augmented AIi^2, ie including priors (default: False)
+@param augmented: bool, flag to calculate the augmented AIi^2, ie including priors (default: False)
 
             This function is used to set the parameters
             self.chi2
@@ -616,6 +650,81 @@ class FitResult:
             self.aug_AIC    = self.AIC_from_lsqfit(nlf, augmented = True)
             
             self.prior      = nlf.prior
+
+    def import_from_linear_regression(self, target_data, result_params, design_matrix, weight_matrix, parameter_names, nres = None):
+        # deduce if we have an intercept or not by checking how many parameters we have
+        has_intercept = len(result_params) == 2
+
+        if self.fcn is None:
+            if has_intercept:
+                self.fcn = lambda x,p: x*p[parameter_names[0]] + p[parameter_names[1]]
+            else:
+                self.fcn = lambda x,p: x*p[parameter_names[0]]
+
+        if self.dof is None:
+            self.dof = np.abs(self.te-self.ts) - len(result_params)
+
+            
+
+        if nres is not None:
+            # check that the dictionary is set and fillable
+            if not bool(self.best_fit_param_res):
+                self.best_fit_param_res = {}
+
+            # calculate Gaussian error propagation
+            cov = np.linalg.inv(design_matrix.T @ weight_matrix @ design_matrix)
+            
+            for key_id,key in enumerate(parameter_names):
+                # check if the resample array exists. If not set it
+                # dtype = object allows to store gvar.gvar instances 
+                if key not in self.best_fit_param_res.keys():
+                    self.best_fit_param_res[key] = np.empty(self.Nres, dtype=object)
+
+                self.best_fit_param_res[key][nres] = gv.gvar(
+                    result_params[key_id],
+                    np.sqrt(cov[key_id,key_id])
+                )
+
+            result = design_matrix @ result_params
+            residuals = target_data - result
+            self.chi2_res[nres] = residuals.T @ weight_matrix @ residuals # no priors in this fit
+            self.aug_chi2_res[nres] = self.chi2
+
+            F_statistic = (target_data.T @ weight_matrix @ target_data - residuals.T @ weight_matrix @ residuals) / 2 / (self.chi2/self.dof)
+            self.Q_value_res[nres] = 1 - scipy.stats.f.cdf(F_statistic, 2, self.dof)
+
+            self.AIC_res[nres] = self.calculate_AIC( self.chi2 )
+            self.aug_AIC_res[nres] = self.AIC
+
+            self.prior_res = None
+
+        # central value fit
+        else: 
+            if not bool(self.best_fit_param):
+                self.best_fit_param = {}
+
+            # calculate Gaussian error propagation
+            cov = np.linalg.inv(design_matrix.T @ weight_matrix @ design_matrix)
+            
+            for key_id,key in enumerate(parameter_names):
+                self.best_fit_param[key] = gv.gvar(
+                    result_params[key_id],
+                    np.sqrt(cov[key_id,key_id])
+                )
+
+            result = design_matrix @ result_params
+            residuals = target_data - result
+            self.chi2 = residuals.T @ weight_matrix @ residuals # no priors in this fit
+            self.aug_chi2 = self.chi2
+
+            F_statistic = (target_data.T @ weight_matrix @ target_data - residuals.T @ weight_matrix @ residuals) / 2 / (self.chi2/self.dof)
+            self.Q_value = 1 - scipy.stats.f.cdf(F_statistic, 2, self.dof)
+
+            self.AIC = self.calculate_AIC( self.chi2 )
+            self.aug_AIC = self.AIC
+
+            self.prior = None
+        # end else
 
 # end of class: FitResult 
 
--- correlatoranalyser/linearRegression.py
+++ correlatoranalyser/linearRegression.py
@@ -2,15 +2,19 @@ import numpy as np
 
 import gvar as gv
 
-import lsqfit
+import h5py
 
-import warnings
+from dataclasses import dataclass, field, fields
 
-from collections.abc import Callable
+from pathlib import Path
+
+from typing import Self, List, Dict
 
 from .fitResult import FitResult
 
-def fit(
+# # TODO: Project Generalize
+
+def linear_regression(     
     *,
     abscissa: np.ndarray,
     ordinate_est: np.ndarray[gv.GVar] | None = None,
@@ -24,43 +28,20 @@ def fit(
     central_value_fit_correlated: bool = False,
     resample_fit: bool = False,
     resample_fit_correlated: bool = False,
-    resample_fit_resample_prior: bool = True,
     resample_type: str | None = None,
-    # args for lsqfit:
-    model: Callable | None = None,
-    prior: dict | None = None,
-    p0: dict | None = None,
-    svdcut: float | None = None,
-    maxiter: int = 10_000,
-) -> FitResult:
-    r"""!
-        @param abscissa: datapoints for the x-axis (i.e. an array containing Nt times (shape (Nt,))
-        @param ordinate_est: datapoints for the y-axis (i.e. an array containing the datapoints measured at Nt times (shape (Nt,))
-        @param ordinate_std: standard deviation of the given y datapoints (i.e. an array containing the error of the measured datapoints (shape (Nt,))
-        @param ordinate_cov: covariance matrix of the y datapoints to specify the correlation betweem them
-        @param resample_ordinate_est: datapoints for the y-axis for each resample (array of shape (Nres,Nt))
-        @param resample_ordinate_std: standard deviation of the datapoints for each resample (array of shape (Nres,Nt) or (Nt,), the latter uses the given variance for all resamples)
-        @param resample_ordinate_cov: covariance matrix of the datapoints for each resample (array of shape (Nres,Nt,Nt) or (Nt,Nt), the latter uses the given covariance matrix for all resamples)
-
-        @param central_value_fit: option whether a central value fit should be performed (default: True)
-        @param central_value_fit_correlated: option whether correlated fit should be performed (default: False)
-        @param resample_fit: option whether a resample fit should be performed (delfault: False)
-        @param resample_ft_corrrelated: option whether a correlated resample fit should be performed (default: False)
-        @param resample_fit_resample_prior: option whether the meanvalue of the prior should be resampled for each resample, without resampling (option 'False') the same meanvalue for the prior is used for all resamples (default: True)
-        @param resample_type: a string representing a resample type (None, 'bst' bootstrap, 'jkn' jackknife). This is passed to the FitResult to determine error calculation.
-
-        @param model: the function to be fit to the datapoints, arguments should be the abscissa and the fit parameters
-        @param prior: a priori estimates for the fit parameters (default: None)
-        @param p0: start value for the fit parameters (default:None)
-        @param maxiter: the maximum of iterations to perform the fit (default: 10_000)
-        This function peforms a fit using lsqfit by Peter Lapage. Default is an uncorrelated central value fit.
-        Optional are a correlated central value fit and a correlated (uncorrelated) resample fit. The the best parameters for the fit are then returned
-    """
-
+    has_intercept: bool = True,
+    parameter_names: tuple | None = None 
+):
     # Ensure that we got at least one fitting strategy (both are possible and will be handled accordingly)
     if not (central_value_fit or resample_fit):
         raise ValueError(f"At least one fit strategy needs to be defined: central_value_fit or resample_fit")
 
+    if parameter_names is None:
+        # slope, (intercept)
+        if has_intercept:
+            parameter_names = ["m", "b"]
+        else:
+            parameter_names = ["m"]
 
     # check if the given arguments have the correct dimensions:
     
@@ -122,6 +103,7 @@ def fit(
     #   - covariance (if correlated)
     #   - Resample types ('bst' by default)
     if resample_fit:
+
         if resample_ordinate_est is None:
             raise ValueError(f"Resampled fit requires resample_ordinate_est")
         
@@ -178,14 +160,6 @@ def fit(
                 if resample_ordinate_cov.shape[0] != N and resample_ordinate_cov.shape[1] != N and resample_ordinate_cov.ndim == 2:
                     raise ValueError(f"Expecting resample_ordinate_cov of shape (Nres,N,N) or (N,N) but has {resample_ordinate_cov.shape}")
     
-    # Check the existence of the model function
-    if model is None:
-        raise ValueError(f"A model for the fit is required, the function should have abscissa and the parameters as an argument")
-
-    # Determine if we work with priors or simple start parameters
-    if prior is None and p0 is None:
-        raise ValueError(f"At least one of prior or p0 needs to be defined")
-    
     # ##############################################################################################
     # ##############################################################################################
     # Now all relevant parameters are there and have the expected shapes. 
@@ -194,21 +168,20 @@ def fit(
     # ##############################################################################################
     # ##############################################################################################
 
-    # prepare the arguments for lsqfit
-    args = {}
+    def lin_reg(y, X = None, W = None, S = None):
+        # if the solution matrix is not given we need X and W to construct it
+        # here we check if X,W are given
+        if (X is None or W is None) and S is None:
+            raise RuntimeError(f"Solution matrix not provided and not possible to construct")
+        # Now we can construct the solution matrix
 
-    # populate the fit function
-    args["fcn"] = model
-
-    # populate a maximal iteration for the minimizer
-    args["maxit"] = maxiter
-
-    # populate the prior/start parameter
-    args["prior" if prior is not None else "p0"] = prior if prior is not None else p0
-
-    # svdcut is optional
-    if svdcut is not None:
-        args["svdcut"] = svdcut
+        if S is None:
+            S = np.linalg.inv(X.T @ W @ X) @ X.T @ W
+        
+        # This returns a np.array of size 1 (2) being slope (intercept)
+        # and the constructed solution matrix, for later use
+        return S @ y, S 
+    # end if lin_reg
 
     # define a FitResult that can be returned
     if resample_fit:
@@ -232,109 +205,78 @@ def fit(
             te=abscissa[-1]
         ) 
 
-    # prepare data for the central value fit:
     if central_value_fit:
-        # for the uncorrelated fit check in which way the standard deviation is given and save in temp
+        # for the uncorrelated fit check in which way the standard deviation is given and compute the weight matrix 
         if not central_value_fit_correlated:
             # simply provided standard deviation (preferred pass)
             if ordinate_std is not None:
-                temp: np.ndarray = ordinate_std
+                weight_matrix: np.ndarray = np.diag(1./ordinate_std**2)
             
             # provided covariance in case standard deviation is not given (preferred pass)
             elif ordinate_cov is not None:
-                temp: np.ndarray = np.diag(ordinate_cov)
+                weight_matrix: np.ndarray = np.diag(1/np.diag(ordinate_cov))
 
             # provided resample standard deviation only, we can attempt to reuse it (optional pass for reusability)
             # if the dimension matches the expected standard deviation (frozen case for resampled fits)
             elif resample_ordinate_std is not None:
                 if resample_ordinate_std.shape[0] == N:
-                    temp: np.ndarray = resample_ordinate_std
+                    weight_matrix: np.ndarray = np.diag(1/resample_ordinate_std)
                 else:
                     raise ValueError(f"No standard deviation specified for central value fit, only resample_ordinate_std with shape {resample_ordinate_var.shape}")
 
             # Same as above but with covariance (optional pass for reusability)
             elif resample_ordinate_cov is not None:
                 if resample_ordinate_cov.shape == (N, N):
-                    temp: np.ndarray = np.diag(resample_ordinate_cov)
+                    weight_matrix: np.ndarray = np.diag(1/np.diag(resample_ordinate_cov))
                 else:
                     raise ValueError(f"No standard devation given and could not be extracted from resample_ordinate_cov with shape {resample_ordinate_cov.shape}")
             
             # standard deviation not provided, doing an unweighted fit 
             else:
-                temp: np.ndarray = np.ones_like(ordinate_est)
+                weight_matrix: np.ndarray = np.eye(N)
 
 
         # for the correlated fit check in which way the covariance  is given and save in temp
         else:
             # simply provided covariance (preferred pass)
             if ordinate_cov is not None:
-                temp: np.ndarray = ordinate_cov
+                weight_matrix: np.ndarray = np.linalg.inv(ordinate_cov)
             # provided resample covariance only, we can attempt to reuse it (optional pass for reusability)
             # if the dimension matches the expected covariance (frozen case for resampled fits)
             elif resample_ordinate_cov is not None:
                 if resample_ordinate_cov.shape == (N, N):
-                    temp: np.ndarray = resample_ordinate_cov
+                    weight_matrix: np.ndarray = np.linalg.inv(resample_ordinate_cov)
                 else:
                     raise ValueError(f"No covariance specified for central value fit, only resample_ordinate_cov with shape {resample_ordinate_cov.shape}")
             else:
                 raise ValueError(f"No covariance given and could not be extracted")
 
-        ordinate_gvar = gv.gvar(
-            # at least one is provided as checked above
-            ordinate_est if ordinate_est is not None else np.mean(resample_ordinate_est, axis=0),
-            # put the standard deviation/covariance as extraced into temp
-            temp,
+        design_matrix: np.ndarray = np.column_stack((-abscissa, np.ones_like(abscissa))) if has_intercept else -abscissa.reshape(-1, 1)
+
+        result_params, solution_matrix = lin_reg(
+            y = ordinate_est if ordinate_est is not None else np.mean(resample_ordinate_est, axis=0), 
+            X = design_matrix, 
+            W = weight_matrix, 
+            S = None
         )
 
-        # Ensure un-/correlated fits are preformed by providing the correct data form to lsqfit
-        # data :   correlated fit
-        # udata: uncorrelated fit
-        args["data" if central_value_fit_correlated else "udata"] = (abscissa, ordinate_gvar)
+        fit_result.import_from_linear_regression(
+            target_data = ordinate_est if ordinate_est is not None else np.mean(resample_ordinate_est, axis=0),
+            result_params = result_params,
+            design_matrix = design_matrix,
+            weight_matrix = weight_matrix,
+            parameter_names = parameter_names,
+            nres = None
+        )
 
+    # end if central_value_fit
 
-        # ##############################################################################################
-        # Now all required fields in args are populated to attempt a fit 
-        # ##############################################################################################
-
-        try:
-            # Save the fit results in fit_result (object of type FitResult)
-            fit_result.import_from_lsqfit(nlf=lsqfit.nonlinear_fit(**args))  
-        
-        # In case something goes wrong we collect additional information and extend the exception message 
-        except Exception as e:
-            msg = f"Fit Failed: :\n"
-            for key, val in args.items():
-                msg += f"- {key}: {val}\n"
-            raise RuntimeError(f"{msg}\n{e}")
-
-
-    # end if central value fit
-    
-    # If no resampled fits are supposed to be done we can return here
     if not resample_fit:
-        # return fit results
         return fit_result
 
-    # prepare the arguments for lsqfit
-    args = {}
+    # cache the solution matrix if desired!
+    solution_matrix = None 
 
-    # populate the fit function
-    args["fcn"] = model
-
-    # populate a maximal iteration for the minimizer
-    args["maxit"] = maxiter
-
-    # populate the prior/start parameter
-    # This may be resampled according to resample_fit_resample_prior
-    args["prior" if prior is not None else "p0"] = prior if prior is not None else p0
-
-    # svdcut is optional
-    if svdcut is not None:
-        args["svdcut"] = svdcut
-
-
-
-    # prepare data for a resample fit
     for nres in range(Nres):
         # for uncorrelated resampled fits check in which way the standard is given and save in temp
         if not resample_fit_correlated:
@@ -342,78 +284,66 @@ def fit(
             if resample_ordinate_std is not None:
                 # frozen error: one for all
                 if resample_ordinate_std.shape[0] == N:
-                    temp: np.ndarray = resample_ordinate_std
+                    frozen_weight_matrix:bool = True
+
+                    weight_matrix: np.ndarray = np.diag(1/resample_ordinate_std)
                 # one std for each resample
                 elif resample_ordinate_std.shape[0] == Nres:
-                    temp: np.ndarray = resample_ordinate_std[nres]
+                    frozen_weight_matrix:bool = False
+
+                    weight_matrix: np.ndarray = np.diag(1/resample_ordinate_std[nres])
                 else:
                     raise ValueError(f"Couldn't identify resample standard deviation from provided resample_ordinate_std of shape {resample_ordinate_std.shape}")
             # provided covariance in case standard deviation is not given
             elif resample_ordinate_cov is not None:
                 # frozen error: one for all
                 if resample_ordinate_cov.shape[0] == N and resample_ordinate_cov.shape[1] == N:
-                    temp: np.ndarray = np.diag(resample_ordinate_cov)
+                    frozen_weight_matrix:bool = True
+                    weight_matrix: np.ndarray = np.diag(1/np.diag(resample_ordinate_cov))
+
                 # one std for each resample
                 elif resample_ordinate_cov.shape[0] == Nres and resample_ordinate_cov.shape[1] == N and resample_ordinate_cov.shape[2] == N:
-                    temp: np.ndarray = np.diag(resample_ordinate_cov[nres,:,:])
+                    frozen_weight_matrix:bool = False
+                    weight_matrix: np.ndarray = np.diag(1/np.diag(resample_ordinate_cov[nres,:,:]))
                 else:
                     raise ValueError(f"Couldn't identify resample standard deviation from provided resample_ordinate_cov of shape {resample_ordinate_cov.shape}")
 
             # standard deviation not provided, doing an unweighted fit 
             else:
-                temp: np.ndarray = np.ones_like(resample_ordinate_est[nres])
+                frozen_weight_matrix:bool = True
+                weight_matrix: np.ndarray = np.eye(N)
 
         # for a correlated resample fit check in which way the covariance is given and save in temp
         else:
             # simply provided covariance
             # frozen covariance: one for all
             if resample_ordinate_cov.shape[0] == N and resample_ordinate_cov.shape[1] == N:
-                temp = resample_ordinate_cov
+                frozen_weight_matrix:bool = True
+                weight_matrix:np.ndarray = np.linalg.inv(resample_ordinate_cov)
             # one covariance per resample
             elif resample_ordinate_cov.shape[0] == Nres and resample_ordinate_cov.shape[1] == N and resample_ordinate_cov.shape[2] == N:
-                temp = resample_ordinate_cov[nres]
+                frozen_weight_matrix:bool = False
+                weight_matrix:np.ndarray = np.linalg.inv(resample_ordinate_cov[nres])
             else:
                 raise ValueError(f"Couldn't identify resample covariance from provided resample_ordinate_cov of shape {resample_ordinate_cov.shape}")
 
+        design_matrix: np.ndarray = np.column_stack((-abscissa, np.ones_like(abscissa))) if has_intercept else -abscissa.reshape(-1, 1)
 
-        ordinate_gvar = gv.gvar(
-            # each resample has its own ordinate data    
-            resample_ordinate_est[nres, :], 
-            # and deduced uncertainty
-            temp
+        result_params, solution_matrix = lin_reg(
+            y = ordinate_est if ordinate_est is not None else np.mean(resample_ordinate_est, axis=0), 
+            X = design_matrix, 
+            W = weight_matrix, 
+            S = solution_matrix if frozen_weight_matrix else None
         )
 
-        # Ensure un-/correlated fits are preformed by providing the correct data form to lsqfit
-        # data :   correlated fit
-        # udata: uncorrelated fit
-        args["data" if resample_fit_correlated else "udata"] = (abscissa, ordinate_gvar)
-
-        # varying the prior mean value for each resample sample, to avoid bias
-        if prior is not None and resample_fit_resample_prior:
-            prior_res = gv.BufferDict()
-
-            for key in prior.keys():
-                prior_res[key] = gv.gvar(gv.sample(prior[key], 1), prior[key].sdev)
-            
-            args["prior"] = prior_res
-
-        # ##############################################################################################
-        # Now all required fields in args are populated to attempt a fit 
-        # ##############################################################################################
-            
-        try:
-            # Save the fit results in fit_result (object of type FitResult)
-            fit_result.import_from_lsqfit(nlf = lsqfit.nonlinear_fit(**args), nres = nres)
-
-        # In case something goes wrong we collect additional information and extend the exception message 
-        except Exception as e:
-            msg = f"Fit Failed: :\n"
-            for key, val in args.items():
-                msg += f"- {key}: {val}\n"
-            msg += f"- nres: {nres}\n"
-            raise RuntimeError(f"{msg}\n{e}")
-
+        fit_result.import_from_linear_regression(
+            target_data = ordinate_est if ordinate_est is not None else np.mean(resample_ordinate_est, axis=0),
+            result_params = result_params,
+            design_matrix = design_matrix,
+            weight_matrix = weight_matrix,
+            parameter_names = parameter_names,
+            nres = nres
+        )
 
     return fit_result
-
 
